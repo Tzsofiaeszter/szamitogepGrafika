a29f29d024c62a211d7771bdc13a22e83536d166
Tzsofiaeszter <146532451+Tzsofiaeszter@users.noreply.github.com>
2025-05-20 16:17:34 +0200

    Delete game_state.c

--- game_state.c
+++
@@ -1,8 +0,0 @@
-#include "game_state.h"
-
-void game_state_init(GameState* state) {
-    state->running = true;
-    state->show_help = false;
-    state->show_minimap = false;
-    state->brightness = 255; // maximális fényerő induláskor
-}
