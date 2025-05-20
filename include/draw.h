f43ea93a760f864ea2365f33ba1621ecb88dc9db
Tzsofiaeszter <146532451+Tzsofiaeszter@users.noreply.github.com>
2025-05-20 16:14:47 +0200

    Delete include directory

--- include/draw.h
+++
@@ -1,9 +0,0 @@
-#ifndef OBJ_DRAW_H
-#define OBJ_DRAW_H
-
-#include "model.h"
-
-void draw_model(const Model* model);
-void draw_triangles(const Model* model);
-
-#endif 
--- include/game.h
+++
@@ -1,9 +0,0 @@
-#pragma once   // - a fordítás során csak eygsezr szabad felhasználni
-
-#include <SDL2/SDL.h>
-
-void game_init();                           // A játék inicializálása (erőforrások betöltése, kezdőállapot)
-void game_update();                         // A játék aktuális logikájának frissítése
-void game_handle_event(SDL_Event* event);   // Események (input) kezelése – például SDL billentyűleütések
-void game_cleanup();                        // A játék erőforrásainak felszabadítása
-bool game_is_running();                     // Ellenőrzi, hogy a játék még fut-e (főciklusban használandó)
